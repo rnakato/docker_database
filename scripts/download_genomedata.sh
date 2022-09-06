3403cfd7a8b66bbf77c057cc71b8aeb91ad6beee
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2022-09-06 09:12:52 +0900

    update to allow UCSC

--- scripts/download_genomedata.sh
+++ scripts/download_genomedata.sh
@@ -32,7 +32,7 @@ ex(){
     eval $1
 }
 
-echo "Start downloading. Selected build: $build"
+echo "Start downloading. Selected genome build: $build"
 
 wget="wget -nv --timestamping"
 
@@ -41,17 +41,25 @@ download_mappability(){
     for k in 28 36 50
     do
         $wget https://www.nakatolab.iqb.u-tokyo.ac.jp/DockerDatabase/mappability/${label}_mappability_Mosaics_${k}mer.tar.bz2
-        tar xvfj ${label}_mappability_Mosaics_${k}mer.tar.bz2
+        tar xvfj ${label}_mappability_Mosaics_${k}mer.tar.bz2 >& /dev/null
         rm ${label}_mappability_Mosaics_${k}mer.tar.bz2
     done
+}
+
+download_genome2bit(){
+    build=$1
+    url=https://hgdownload.soe.ucsc.edu/goldenPath/$build/bigZips
+    genome=$url/$build.2bit
+    gt=$url/$build.chrom.sizes
+    ex "wget -nv $genome -O genome_full.2bit"
+    ex "wget -nv $gt    -O genometable_full.txt"
 }
 
 mkdir -p $outputdir && cd $_
 Ensembl_version=106
 
 if test $build = "GRCh38" -o $build = "hg38"; then
-    ex "$wget https://hgdownload.soe.ucsc.edu/goldenPath/hg38/bigZips/hg38.2bit -O genome_full.2bit"
-    ex "$wget https://hgdownload.soe.ucsc.edu/goldenPath/hg38/bigZips/hg38.chrom.sizes -O genometable_full.txt"
+    download_genome2bit hg38
     ex "$wget http://ftp.ensembl.org/pub/release-$Ensembl_version/gtf/homo_sapiens/Homo_sapiens.GRCh38.$Ensembl_version.chr.gtf.gz"
     ex "$wget http://ftp.ensembl.org/pub/release-$Ensembl_version/gff3/homo_sapiens/Homo_sapiens.GRCh38.$Ensembl_version.chr.gff3.gz"
 #    ex "$wget http://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_40/gencode.v40.annotation.gtf.gz"
@@ -63,8 +71,7 @@ if test $build = "GRCh38" -o $build = "hg38"; then
     download_mappability Ensembl-GRCh38
     chrs="$(seq 1 22) X Y M"
 elif test $build = "GRCh37" -o $build = "hg19"; then
-    ex "$wget https://hgdownload.soe.ucsc.edu/goldenPath/hg19/bigZips/hg19.2bit -O genome_full.2bit"
-    ex "$wget https://hgdownload.soe.ucsc.edu/goldenPath/hg19/bigZips/hg19.chrom.sizes -O genometable_full.txt"
+    download_genome2bit hg19
     ex "$wget http://ftp.ensembl.org/pub/grch37/release-$Ensembl_version/gtf/homo_sapiens/Homo_sapiens.GRCh37.87.chr.gtf.gz"
     ex "$wget http://ftp.ensembl.org/pub/grch37/release-$Ensembl_version/gff3/homo_sapiens/Homo_sapiens.GRCh37.87.chr.gff3.gz"
     ex "$wget http://ftp.ensembl.org/pub/grch37/release-$Ensembl_version/fasta/homo_sapiens/cdna/Homo_sapiens.GRCh37.cdna.all.fa.gz"
@@ -76,7 +83,7 @@ elif test $build = "GRCh37" -o $build = "hg19"; then
 elif test $build = "T2T"; then
     # https://genomeinformatics.github.io/CHM13v2/
     # https://github.com/marbl/chm13
-    ex "$wget https://s3-us-west-2.amazonaws.com/human-pangenomics/T2T/CHM13/assemblies/analysis_set/chm13v2.0.fa.gz -O genome.fa.gz"
+    ex "wget -nv https://s3-us-west-2.amazonaws.com/human-pangenomics/T2T/CHM13/assemblies/analysis_set/chm13v2.0.fa.gz -O genome.fa.gz"
     ex "$wget https://s3-us-west-2.amazonaws.com/human-pangenomics/T2T/CHM13/assemblies/annotation/chm13.draft_v2.0.cen_mask.bed"
     ex "unpigz -f genome.fa.gz"
     zcat /opt/T2Tdata/chm13v2.gtf.gz > chm13v2.gtf
@@ -84,8 +91,7 @@ elif test $build = "T2T"; then
     download_mappability T2T
     chrs="$(seq 1 22) X Y M"
 elif test $build = "GRCm39" -o $build = "mm39"; then
-    ex "$wget https://hgdownload.soe.ucsc.edu/goldenPath/mm39/bigZips/mm39.2bit -O genome_full.2bit"
-    ex "$wget https://hgdownload.soe.ucsc.edu/goldenPath/mm39/bigZips/mm39.chrom.sizes -O genometable_full.txt"
+    download_genome2bit mm39
     ex "$wget http://ftp.ensembl.org/pub/release-$Ensembl_version/gtf/mus_musculus/Mus_musculus.GRCm39.$Ensembl_version.chr.gtf.gz"
     ex "$wget http://ftp.ensembl.org/pub/release-$Ensembl_version/gff3/mus_musculus/Mus_musculus.GRCm39.$Ensembl_version.chr.gff3.gz"
     ex "$wget http://ftp.ensembl.org/pub/release-$Ensembl_version/fasta/mus_musculus/cdna/Mus_musculus.GRCm39.cdna.all.fa.gz"
@@ -94,8 +100,7 @@ elif test $build = "GRCm39" -o $build = "mm39"; then
     download_mappability Ensembl-GRCm39
     chrs="$(seq 1 19) X Y M"
 elif test $build = "GRCm38" -o $build = "mm10"; then
-    ex "$wget https://hgdownload.soe.ucsc.edu/goldenPath/mm10/bigZips/mm10.2bit -O genome_full.2bit"
-    ex "$wget https://hgdownload.soe.ucsc.edu/goldenPath/mm10/bigZips/mm10.chrom.sizes -O genometable_full.txt"
+    download_genome2bit mm10
     ex "$wget http://ftp.ensembl.org/pub/release-102/gtf/mus_musculus/Mus_musculus.GRCm38.102.chr.gtf.gz"
     ex "$wget http://ftp.ensembl.org/pub/release-102/gff3/mus_musculus/Mus_musculus.GRCm38.102.chr.gff3.gz"
     ex "$wget http://ftp.ensembl.org/pub/release-102/fasta/mus_musculus/cdna/Mus_musculus.GRCm38.cdna.all.fa.gz"
@@ -105,8 +110,7 @@ elif test $build = "GRCm38" -o $build = "mm10"; then
     download_mappability Ensembl-GRCm38
     chrs="$(seq 1 19) X Y M"
 elif test $build = "mRatBN7.2" -o $build = "rn7"; then
-    ex "$wget https://hgdownload.soe.ucsc.edu/goldenPath/rn7/bigZips/rn7.2bit -O genome_full.2bit"
-    ex "$wget https://hgdownload.soe.ucsc.edu/goldenPath/rn7/bigZips/rn7.chrom.sizes -O genometable_full.txt"
+    download_genome2bit rn7
     ex "$wget http://ftp.ensembl.org/pub/release-$Ensembl_version/gtf/rattus_norvegicus/Rattus_norvegicus.mRatBN7.2.$Ensembl_version.chr.gtf.gz"
     ex "$wget http://ftp.ensembl.org/pub/release-$Ensembl_version/gff3/rattus_norvegicus/Rattus_norvegicus.mRatBN7.2.$Ensembl_version.chr.gff3.gz"
     ex "$wget http://ftp.ensembl.org/pub/release-$Ensembl_version/fasta/rattus_norvegicus/cdna/Rattus_norvegicus.mRatBN7.2.cdna.all.fa.gz"
@@ -115,8 +119,7 @@ elif test $build = "mRatBN7.2" -o $build = "rn7"; then
     download_mappability Ensembl-mRatBN7.2
     chrs="$(seq 1 20) X Y M"
 elif test $build = "GRCz11" -o $build = "danRer11"; then
-    ex "$wget https://hgdownload.soe.ucsc.edu/goldenPath/danRer11/bigZips/danRer11.2bit -O genome_full.2bit"
-    ex "$wget https://hgdownload.soe.ucsc.edu/goldenPath/danRer11/bigZips/danRer11.chrom.sizes -O genometable_full.txt"
+    download_genome2bit danRer11
     ex "$wget http://ftp.ensembl.org/pub/release-$Ensembl_version/gtf/danio_rerio/Danio_rerio.GRCz11.$Ensembl_version.chr.gtf.gz"
     ex "$wget http://ftp.ensembl.org/pub/release-$Ensembl_version/gff3/danio_rerio/Danio_rerio.GRCz11.$Ensembl_version.chr.gff3.gz"
     ex "$wget http://ftp.ensembl.org/pub/release-$Ensembl_version/fasta/danio_rerio/cdna/Danio_rerio.GRCz11.cdna.all.fa.gz"
@@ -125,8 +128,7 @@ elif test $build = "GRCz11" -o $build = "danRer11"; then
     download_mappability Ensembl-GRCz11
     chrs="$(seq 1 25) M"
 elif test $build = "GRCg6a" -o $build = "galGal6"; then
-    ex "$wget https://hgdownload.soe.ucsc.edu/goldenPath/galGal6/bigZips/galGal6.2bit -O genome_full.2bit"
-    ex "$wget https://hgdownload.soe.ucsc.edu/goldenPath/galGal6/bigZips/galGal6.chrom.sizes -O genometable_full.txt"
+    download_genome2bit galGal6
     ex "$wget http://ftp.ensembl.org/pub/release-$Ensembl_version/gtf/gallus_gallus/Gallus_gallus.GRCg6a.$Ensembl_version.chr.gtf.gz"
     ex "$wget http://ftp.ensembl.org/pub/release-$Ensembl_version/gff3/gallus_gallus/Gallus_gallus.GRCg6a.$Ensembl_version.chr.gff3.gz"
     ex "$wget http://ftp.ensembl.org/pub/release-$Ensembl_version/fasta/gallus_gallus/cdna/Gallus_gallus.GRCg6a.cdna.all.fa.gz"
@@ -135,8 +137,7 @@ elif test $build = "GRCg6a" -o $build = "galGal6"; then
     download_mappability Ensembl-GRCg6a
     chrs="$(seq 1 28) $(seq 30 33) W Z M"
 elif test $build = "Xenopus_tropicalis" -o $build = "xenLae2"; then
-    ex "$wget https://hgdownload.soe.ucsc.edu/goldenPath/xenLae2/bigZips/xenLae2.2bit -O genome_full.2bit"
-    ex "$wget https://hgdownload.soe.ucsc.edu/goldenPath/xenLae2/bigZips/xenLae2.chrom.sizes -O genometable_full.txt"
+    download_genome2bit xenLae2
     ex "$wget https://hgdownload.soe.ucsc.edu/goldenPath/xenLae2/bigZips/refMrna.fa.gz"
     ex "$wget http://ftp.ensembl.org/pub/release-$Ensembl_version/gtf/xenopus_tropicalis/Xenopus_tropicalis.Xenopus_tropicalis_v9.1.$Ensembl_version.chr.gtf.gz"
     ex "$wget http://ftp.ensembl.org/pub/release-$Ensembl_version/gff3/xenopus_tropicalis/Xenopus_tropicalis.Xenopus_tropicalis_v9.1.$Ensembl_version.chr.gff3.gz"
@@ -146,8 +147,7 @@ elif test $build = "Xenopus_tropicalis" -o $build = "xenLae2"; then
     download_mappability Ensembl-xenLae2
     chrs="1L 1S 2L 2S 3L 3S 4L 4S 5L 5S 6L 6S 7L 7S 8L 8S 9_10L 9_10S M"
 elif test $build = "BDGP6" -o $build = "dm6"; then
-    ex "$wget https://hgdownload.soe.ucsc.edu/goldenPath/dm6/bigZips/dm6.2bit -O genome_full.2bit"
-    ex "$wget https://hgdownload.soe.ucsc.edu/goldenPath/dm6/bigZips/dm6.chrom.sizes -O genometable_full.txt"
+    download_genome2bit dm6
     ex "$wget http://ftp.ensembl.org/pub/release-$Ensembl_version/gtf/drosophila_melanogaster/Drosophila_melanogaster.BDGP6.32.$Ensembl_version.chr.gtf.gz"
     ex "$wget http://ftp.ensembl.org/pub/release-$Ensembl_version/gff3/drosophila_melanogaster/Drosophila_melanogaster.BDGP6.32.$Ensembl_version.chr.gff3.gz"
     ex "$wget http://ftp.ensembl.org/pub/release-$Ensembl_version/fasta/drosophila_melanogaster/cdna/Drosophila_melanogaster.BDGP6.32.cdna.all.fa.gz"
@@ -156,8 +156,7 @@ elif test $build = "BDGP6" -o $build = "dm6"; then
     download_mappability Ensembl-BDGP6
     chrs="2L 2R 3L 3R 4 X Y M"
 elif test $build = "WBcel235" -o $build = "ce11"; then
-    ex "$wget https://hgdownload.soe.ucsc.edu/goldenPath/ce11/bigZips/ce11.2bit -O genome_full.2bit"
-    ex "$wget https://hgdownload.soe.ucsc.edu/goldenPath/ce11/bigZips/ce11.chrom.sizes -O genometable_full.txt"
+    download_genome2bit ce11
     ex "$wget http://ftp.ensembl.org/pub/release-$Ensembl_version/gtf/caenorhabditis_elegans/Caenorhabditis_elegans.WBcel235.$Ensembl_version.gtf.gz"
     ex "$wget http://ftp.ensembl.org/pub/release-$Ensembl_version/gff3/caenorhabditis_elegans/Caenorhabditis_elegans.WBcel235.$Ensembl_version.gff3.gz"
     ex "$wget http://ftp.ensembl.org/pub/release-$Ensembl_version/fasta/caenorhabditis_elegans/cdna/Caenorhabditis_elegans.WBcel235.cdna.all.fa.gz"
@@ -166,8 +165,7 @@ elif test $build = "WBcel235" -o $build = "ce11"; then
     download_mappability Ensembl-WBcel235
     chrs="I II III IV V X M"
 elif test $build = "R64-1-1" -o $build = "sacCer3"; then
-    ex "$wget https://hgdownload.soe.ucsc.edu/goldenPath/sacCer3/bigZips/sacCer3.2bit -O genome_full.2bit"
-    ex "$wget https://hgdownload.soe.ucsc.edu/goldenPath/sacCer3/bigZips/sacCer3.chrom.sizes -O genometable_full.txt"
+    download_genome2bit sacCer3
     ex "$wget http://ftp.ensembl.org/pub/release-$Ensembl_version/gtf/saccharomyces_cerevisiae/Saccharomyces_cerevisiae.R64-1-1.$Ensembl_version.gtf.gz"
     ex "$wget http://ftp.ensembl.org/pub/release-$Ensembl_version/gff3/saccharomyces_cerevisiae/Saccharomyces_cerevisiae.R64-1-1.$Ensembl_version.gff3.gz"
     ex "$wget https://sgd-prod-upload.s3.amazonaws.com/S000212118/SGD_features.tab.20110827.gz"
@@ -178,7 +176,7 @@ elif test $build = "R64-1-1" -o $build = "sacCer3"; then
     download_mappability Ensembl-R64-1-1
     chrs="I II III IV V VI VII VIII IX X XI XII XIII XIV XV XVI M"
 elif test $build = "SPombe"; then
-    ex "$wget https://www.pombase.org/data/genome_sequence_and_features/genome_sequence/Schizosaccharomyces_pombe_all_chromosomes.fa.gz -O genome_full.fa.gz"
+    ex "wget -nv https://www.pombase.org/data/genome_sequence_and_features/genome_sequence/Schizosaccharomyces_pombe_all_chromosomes.fa.gz -O genome_full.fa.gz"
     ex "unpigz -f genome_full.fa.gz"
     ex "sed -i -e 's/>I/>chrI/g' genome_full.fa"
     ex "sed -i -e 's/mitochondrial/chrM/g' genome_full.fa"
